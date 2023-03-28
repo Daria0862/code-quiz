62b729cd4356e32689fafc9656ea1f0daaae715c
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-28 11:38:23 +0100

    added question and scores js

---
+++ question.js
@@ -0,0 +1,26 @@
+const questions = [{
+    question: "What does CSS stand for?", 
+    choices: ["Cascading Style Sheets", "Colourful Style Sheets", "Computer Style Sheets", "Creative Style Sheets"],
+    answer: "Cascading Style Sheets"
+},
+{
+    question: "What is the output of the following code: console.log(2 + '2')?",
+    choices: ["22", "Error", "4", "2"],
+    answer: "22"
+},
+{
+    question: "Which of the following is NOT a programming language?    ",
+    choices: ["HTML", "CSS", "Python", "JPEG"],
+    answer: "JPEG"
+},
+  {
+    question: "What is the output of the following code: console.log(typeof null)?",
+    choices: ["object", "undefined", "null", "Error"],
+    answer: "object"
+},
+  {
+    question: "What is the output of the following code: console.log(typeof null)?",
+    choices: ["Ruby on Rails", "Angular", "Laravel", "Flask"],
+    answer: "Angular"
+}
+];
---
+++ scores.js
@@ -0,0 +1,15 @@
+const scores = [
+    {
+      name: "Fred",
+      score: 17
+    },
+    {
+      name: "Mildred",
+      score: 18
+    },
+    {
+      name: "Bertha",
+      score: 14
+    }
+  ];
+  
