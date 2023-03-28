ffe7fe2479dc92ea8f748daa8d16f297b6a82883
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-28 13:48:00 +0100

    qs, choices nad score

--- script.js
+++ script.js
@@ -1,48 +1,82 @@
+let currentQuestionIndex = 0;
+let time = 60;
+let timerId;
+let score = 0;
+
+const startBtn = document.getElementById("start");
 const questionTitle = document.getElementById("question-title");
 const choices = document.getElementById("choices");
+const feedback = document.getElementById("feedback");
+const timeEl = document.getElementById("time");
+const endScreen = document.getElementById("end-screen");
+const scoreEl = document.getElementById("score");
 
-let currentQuestionIndex = 0;
+startBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
-  document.getElementById("start-screen").classList.add("hide");
-  document.getElementById("questions").classList.remove("hide");
+    document.getElementById("start-screen").classList.add("hide");
+    document.getElementById("questions").classList.remove("hide");
 
-  showQuestion();
+    timeEl.textContent = time;
+    timerId = setInterval(updateTimer, 1000);
+
+    showQuestion();
 }
 
 function showQuestion() {
-  const currentQuestion = questions[currentQuestionIndex];
+    const currentQuestion = questions[currentQuestionIndex];
 
-  questionTitle.textContent = currentQuestion.question;
+    questionTitle.textContent = currentQuestion.question;
 
-  choices.innerHTML = "";
+    choices.innerHTML = "";
 
-  currentQuestion.choices.forEach(choice => {
-    const button = document.createElement("button");
-    button.textContent = choice;
-    button.classList.add("choice");
-    button.addEventListener("click", checkAnswer);
-    choices.appendChild(button);
-  });
+    currentQuestion.choices.forEach(choice => {
+        const button = document.createElement("button");
+        button.textContent = choice;
+        button.classList.add("choice");
+        button.addEventListener("click", checkAnswer);
+        choices.appendChild(button);
+    });
 }
 
 function checkAnswer(event) {
-  const selectedAnswer = event.target.textContent;
+    const selectedAnswer = event.target.textContent;
 
-  const currentQuestion = questions[currentQuestionIndex];
+    const currentQuestion = questions[currentQuestionIndex];
 
-  if (selectedAnswer === currentQuestion.answer) {
-    console.log("Correct!");
-    currentQuestionIndex++;
-    if (currentQuestionIndex === questions.length) {
-      console.log("Quiz over!");
+    if (selectedAnswer === currentQuestion.answer) {
+        feedback.textContent = "Correct!";
+        score += 10;
+        currentQuestionIndex++;
+        if (currentQuestionIndex === questions.length) {
+            endQuiz();
+        } else {
+            showQuestion();
+        }
     } else {
-      showQuestion();
+        feedback.textContent = "Wrong!";
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+            endQuiz();
+        }
+        updateTimer();
     }
-  } else {
-    console.log("Wrong!");
-  }
 }
 
-const startBtn = document.getElementById("start");
-startBtn.addEventListener("click", startQuiz);
+function updateTimer() {
+    time--;
+    timeEl.textContent = time;
+    if (time === 0) {
+        endQuiz();
+    }
+}
+
+function endQuiz() {
+    clearInterval(timerId);
+
+    document.getElementById("questions").classList.add("hide");
+    endScreen.classList.remove("hide");
+
+    scoreEl.textContent = score;
+}
